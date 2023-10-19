26e318e4feb4767353dd350072b8161e5991c458
GonzaMMR <gonzalomoratomartin@gmail.com>
2023-10-19 10:51:14 +0200

    nuevo

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
+from utils import files
+
 # import your functions here
-print("HELLO")
-# read the quijote here
 
-
+book1 = files.readFile("el_quijote.txt")
+book2 = files.readFile("el_quijote_ii.txt")
+print(book1 + book2)
 # Word Count
 # print('Word Count: ', files.wordCount(book))
+print('word Count', files.wordCount(book1 + book2))
 
 # Unique Word Count
 # print('Unique Word Count: ', files.uniqueWordCount(book))
